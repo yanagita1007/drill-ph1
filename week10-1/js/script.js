c94216c2aa2c7216585d6f9b0e70ad351ea9fa6f
yanagita1007 <1007yanagita@gmail.com>
2023-08-14 11:08:18 +0900

    ボタンを押して文字変更

--- week10-1/js/script.js
+++ week10-1/js/script.js
@@ -1,9 +1,7 @@
 // ボタンと文字のidを取得しましょう
-
+const btn = document.getElementById('changeButton');
+const text = document.getElementById('text');
 // 取得した要素に .onclick で処理を追記しましょう
-
-/*
-.onclick = function() {
-  
+btn.onclick = () => {
+    text.innerHTML = 'Hello, JavaScript!';
 }
-*/
