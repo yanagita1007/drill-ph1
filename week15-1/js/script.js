6d4fc8e1e56d5cdbffcdcf3bc0a36fca37248c86
yanagita1007 <1007yanagita@gmail.com>
2023-10-04 23:29:05 +0900

    ボタンによって記事の表示非表示を変更

--- week15-1/js/script.js
+++ week15-1/js/script.js
@@ -5,6 +5,8 @@ const toggleButtonElement = document.querySelector("#toggle-button");
 
 const toggleArticleVisibility = () => {
   // element.style.display プロパティを用いて、記事が表示されている場合は非表示に、非表示の場合は表示にする処理を書いてください
+  articleElement.style.display = (articleElement.style.display=="") ? "none" : "";
 };
 
 // ボタンをクリックしたら toggleArticleVisibility を実行する
+toggleButtonElement.addEventListener("click", () => toggleArticleVisibility());
