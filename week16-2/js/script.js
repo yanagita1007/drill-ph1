c1a6d422c6ca576de6af5e7bbe8299381fc2490f
yanagita1007 <1007yanagita@gmail.com>
2023-10-13 23:50:00 +0900

    更新ボタンの位置と表示非表示を変更

--- week16-2/js/script.js
+++ week16-2/js/script.js
@@ -7,11 +7,23 @@ let currentEditingElement = null;
 document.querySelectorAll('.edit-button').forEach((btn) => {
   btn.addEventListener('click', function() {
     // ここを追加しましょう
+    updateButtonElement.style.display = "";
+    const box = document.getElementById("box");
+    box.classList.add ("flex");
+    box.classList.add ("flex-col");
+    titleInputElement.classList.add ("w-full");
+    updateButtonElement .classList.add ("w-full")
+    const textElement = btn.parentElement.querySelector('span');
+    titleInputElement.value = textElement.textContent;
+    currentEditingElement = textElement;
   });
 });
 
 updateButtonElement.addEventListener('click', function() {
   if (currentEditingElement) {
     // ここを追加しましょう
+    const textTitle = titleInputElement.value;
+    currentEditingElement.textContent = textTitle;
+    updateButtonElement.style.display = "none";
   }
 });
